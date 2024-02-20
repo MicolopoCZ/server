935094b828453d53bb51a09f49ae350399605e63
MicolopoCZ <armymisa@gmail.com>
2024-02-20 08:35:36 +0100

    server

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io")
+const PORT = 3000;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    }
+});
+
+//id {x, y}
+
+const users = new Map();
+
+
+
+
+io.on("connection", (socket) => {
+    console.log(`Client connected from ${socket.handshake.address}, ${socket.handshake.time}`)
+    socket.on("spawn", (data) => {
+      socket.data.user = data.id
+      users.set(socket.data.user, data.position)
+      io.emit("new user connected", [...users])
+      console.log(users)
+    })
+    socket.on("disconnect", () => {
+      console.log(`${socket.data.user} disconnected`)
+      users.delete(socket.data.user)
+      io.emit("user disconnected", socket.data.user)
+      console.log(users)
+    })
+    socket.on("playerPos", (data) => {
+      users.set(data.id, data.position)
+      console.log(users)
+      io.emit("playerPos", data)
+    })
+})
+
+
+
+
+
+
+server.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
+
